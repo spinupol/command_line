1e28765a781638af7bd9f2f8c93d9af871798012
Sailendra Pinupolu <Spinupolu@gmail.com>
2016-12-03 18:02:13 -0500

    sum_of_column_using_cut_paste.sh

---
+++ sum_of_column_using_cut_paste.sh
@@ -0,0 +1,18 @@
+[app_domt_qa@ip-10-131-66-2 sailendra]$ cat tad.csv
+Reynold,Schmeler,194000
+Kayli,Stamm,118000
+Javon,Funk,102000
+Benedict,Von,39000
+Rowena,Little,113000
+Heaven,Aufderhar,187000
+Nathanial,White,56000
+Wilhelmine,Murazik,73000
+Jace,Kovacek,88000
+Adele,Medhurst,118000
+Garland,Farrell,172000
+[app_domt_qa@ip-10-131-66-2 sailendra]$ a=`cut -d"," -f3 tad.csv | paste -sd+ | bc`
+[app_domt_qa@ip-10-131-66-2 sailendra]$ b=`wc -l < tad.csv`
+[app_domt_qa@ip-10-131-66-2 sailendra]$ echo $a $b
+1260000 11
+[app_domt_qa@ip-10-131-66-2 sailendra]$ echo $a/$b | bc -l
+114545.45454545454545454545
